5ce6d686998690f263164aeccbe3f6fb4c71d406
Diana Salas <salasd10418@gmail.com>
2021-10-27 19:00:17 -0400

    removed excess code for better functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from './components/Header';
+// import Header from './components/Header';
 import Project from './components/Project';
-import Footer from './components/Footer';
-import projects from '../src/projectObjects';
+// import Footer from './components/Footer';
+import projects from './projectList';
 
 function App() {
+  const projectObjs = projects.map(project => {
+    return (
+      <Project project={{ imgUrl: project.imgUrl, title: project.title, deployLink: project.deployedLink, repositoryLink: project.repositoryLink }} />
+    )
+  })
   return (
     <div>
-      <Header />
-      <Project />
-      <Footer />
+      {/* <Header /> */}
+      <Project projects={projects} />
+      {/* <Footer /> */}
     </div>
   );
 }
