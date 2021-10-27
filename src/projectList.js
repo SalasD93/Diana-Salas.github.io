7e41f8bcce746a82b7cb622fe8c58101f007fc2e
Diana Salas <salasd10418@gmail.com>
2021-10-27 18:56:11 -0400

    renamed file / assigned a name to the array

--- src/projectList.js
+++ src/projectList.js
@@ -1,32 +1,44 @@
-export default [
+const projects = [
     {
+        _id: "1",
         imgUrl: "",
+        imgAlt: "",
         title: "",
         deployedLink: "",
         repositoryLink: ""
     },
     {
+        _id: "2",
         imgUrl: "",
+        imgAlt: "",
         title: "",
         deployedLink: "",
         repositoryLink: ""
     },
     {
+        _id: "3",
         imgUrl: "",
+        imgAlt: "",
         title: "",
         deployedLink: "",
         repositoryLink: ""
     },
     {
+        _id: "4",
         imgUrl: "",
+        imgAlt: "",
         title: "",
         deployedLink: "",
         repositoryLink: ""
     },
     {
+        _id: "5",
         imgUrl: "",
+        imgAlt: "",
         title: "",
         deployedLink: "",
         repositoryLink: ""
     }
-]
+];
+
+export default projects;
