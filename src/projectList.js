c4219f365a7707f0bcbf13a512ae840554623bab
Diana Salas <salasd10418@gmail.com>
2021-10-27 20:16:33 -0400

    imported project 1 image / added style object / removed imgAlt / added information to first project

--- src/projectList.js
+++ src/projectList.js
@@ -1,11 +1,13 @@
+import project1Img from "./assets/images/project-1.png";
+
 const projects = [
     {
         _id: "1",
-        imgUrl: "",
-        imgAlt: "",
-        title: "",
-        deployedLink: "",
-        repositoryLink: ""
+        imgUrl: project1Img,
+        style: {width: "400px", height: "200px"},
+        title: "IDK, You Pick!",
+        deployedLink: "https://salasd93.github.io/idk-you-pick/",
+        repositoryLink: "https://github.com/SalasD93/idk-you-pick"
     },
     {
         _id: "2",
