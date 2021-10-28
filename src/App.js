bf07e8aaed842df9351ae0517bb89e71036733dd
Diana Salas <salasd10418@gmail.com>
2021-10-28 15:31:47 -0400

    added conditional page rendering / removed unused imports / header, nav, pages render properly without errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
-import Project from './components/Project';
-// import Footer from './components/Footer';
-import projects from './projectList';
+import Footer from './components/Footer';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Portfolio from './components/pages/Portfolio';
+import Resume from './components/pages/Resume';
 
 function App() {
+  const [currentPage, handlePageChange] = useState('About');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+      default:
+        return <About />;
+    }
+  };
+
   return (
     <div>
-      <Header />
-      <Project projects={projects} />
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      <div>{renderPage(currentPage)}</div>
       {/* <Footer /> */}
     </div>
   );
