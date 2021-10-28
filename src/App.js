155b94e8ea94563d7d375114d640d33336a35566
Diana Salas <salasd10418@gmail.com>
2021-10-28 14:44:35 -0400

    removed unused files / added pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 // import Header from './components/Header';
 import Project from './components/Project';
 // import Footer from './components/Footer';
