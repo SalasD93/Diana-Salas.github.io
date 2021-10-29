491d40211cfd4e73dcb749dbe14db6dac4ba8606
Diana Salas <salasd10418@gmail.com>
2021-10-29 03:18:09 -0400

    added footer and custom css in js styles / footer links function / need to add icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import {
+  Box,
+  Container,
+  Row,
+  FooterLink
+} from './FooterStyles';
 
 function Footer() {
-    return (
-        <div>
-
-        </div>
-    )
-}
+  return (
+    <Box>
+      <Container>
+        <Row>
+            <FooterLink href="https://github.com/SalasD93?tab=repositories" target="_blank">
+              {/* <i className="fab fa-facebook-f"> */}
+                <span style={{ marginLeft: "10px" }}>
+                  GitHub
+                </span>
+              {/* </i> */}
+            </FooterLink>
+            <FooterLink href="https://www.linkedin.com/in/salasdi/" target="_blank">
+              {/* <i className="fab fa-instagram"> */}
+                <span style={{ marginLeft: "10px" }}>
+                  LinkedIn
+                </span>
+              {/* </i> */}
+            </FooterLink>
+            <FooterLink href="mailto:salasd10418@gmail.com">
+              {/* <i className="fab fa-twitter"> */}
+                <span style={{ marginLeft: "10px" }}>
+                  Gmail
+                </span>
+              {/* </i> */}
+            </FooterLink>
+        </Row>
+      </Container>
+    </Box>
+  );
+};
 
 
 export default Footer;
---
+++ src/components/FooterStyles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  padding: 5px 5px;
+  background: #385785;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  
+
+  @media (max-width: 1000px) {
+    padding: 20px 5px;
+  }
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1000px;
+    margin: 0 auto;
+`
+
+export const Row = styled.div`
+  align-self: center;
+  display: grid;
+  grid-template-columns: 100px 100px 100px;
+  grid-gap: 20px;
+  @media (max-width: 1000px) {
+    // grid-template-columns: 60px 25px 25px;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+export const FooterLink = styled.a`
+  color: #fff;
+  margin-bottom: 20px;
+  margin-top: 20px;
+  font-size: 1.25rem;
+  text-decoration: none;
+
+  &:hover {
+      color: #B8E1FF;
+      transition: 200ms ease-in;
+  }
+
+  &:focus {
+    color: #002C45;
+  }
+`;
