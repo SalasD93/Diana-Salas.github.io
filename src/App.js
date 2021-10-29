33de2c678453c192eec04a25684de8e3bd1eee98
Diana Salas <salasd10418@gmail.com>
2021-10-29 00:12:15 -0400

    imported App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/pages/About';
