b3aad18a0691adf6f4d34ea8b4650d8db92c8ba5
Diana Salas <salasd10418@gmail.com>
2021-10-29 00:57:35 -0400

    added class nav-list

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navigation(props) {
     const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
     return (
-        <ul className="d-inline-flex flex-row align-items-end mb-0">
+        <ul className="d-inline-flex flex-row align-items-end mb-0 nav-list">
             {tabs.map(tab => (
                 <li key={tab} className="mx-2">
                     <a
