6ec87592b5003881e6e3334983e2c32341cbc300
Diana Salas <salasd10418@gmail.com>
2021-10-29 00:13:13 -0400

    added style to nav links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,13 @@ import React from 'react';
 function Navigation(props) {
     const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
     return (
-        <ul>
+        <ul className="d-inline-flex flex-row align-items-end mb-0">
             {tabs.map(tab => (
-                <li key={tab}>
+                <li key={tab} className="mx-2">
                     <a
                         href={'#' + tab.toLowerCase()}
                         onClick={() => props.handlePageChange(tab)}
-                        // className={}
+                        className="nav-link pb-0 pt-3"
                     >
                         {tab}
                     </a>
