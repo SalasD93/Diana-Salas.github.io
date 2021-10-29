807de92b03f2aaae2f83d8f732cecea0ea8d5b11
Diana Salas <salasd10418@gmail.com>
2021-10-29 00:56:52 -0400

    added link to brand / added class brand

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Navigation from './Navigation';
 function Header(props) {
     return (
         <div className="d-flex p-2 bg justify-content-between pb-1">
-            <h1 className="align-self-center py-2 rounded">Diana Salas</h1>
+            <a href="/"><h1 className="align-self-center py-2 rounded brand">Diana Salas</h1></a>
             <Navigation currentPage={props.currentPage} handlePageChange={props.handlePageChange}/>
         </div>
     )
