9b48adedbdb97be87c2547b313a5acc621df702b
Diana Salas <salasd10418@gmail.com>
2021-11-03 01:52:21 -0400

    added resume lists

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,13 +1,111 @@
 import React from 'react';
 import Pdf from './Documents/Diana_Salas_Resume_Updated.pdf';
-// import {} from './ResumeStyles';
+import './ResumeStyles.css'
 
 function Resume() {
     return (
-        <div>
-            Resume
-            <a href={Pdf} target="_blank" rel="noreferrer">Download my full resume</a>
-        </div>
+        <section>
+            <h1>Resume</h1>
+            <a href={Pdf} target="_blank" rel="noreferrer">DOWNLOAD RESUME</a>
+            <div id="container">
+                <div className="list-container">
+                <h2>FRONTEND Proficiencies</h2>
+                <ul>
+                    <li>
+                        HTML5
+                    </li>
+                    <li>
+                        CSS3
+                    </li>
+                    <li>
+                        JavaScript/ES6
+                    </li>
+                    <li>
+                        jQuery
+                    </li>
+                    <li>
+                        Responsive Design
+                    </li>
+                    <li>
+                        React
+                    </li>
+                    <li>
+                        XML
+                    </li>
+                    <li>
+                        Bootstrap
+                    </li>
+                    <li>
+                        Search Engine Optimization (SEO)
+                    </li>
+                    <li>
+                        Tailwind
+                    </li>
+                    <li>
+                        Bulma
+                    </li>
+                    <li>
+                        API Integration
+                    </li>
+                </ul>
+                </div>
+                <div className="list-container">
+                <h2>BACKEND Proficiencies</h2>
+                <ul>
+                    <li>
+                        APIs
+                    </li>
+                    <li>
+                        Node
+                    </li>
+                    <li>
+                        Express
+                    </li>
+                    <li>
+                        SQL/MySQL/Sequelize
+                    </li>
+                    <li>
+                        MongoDB/Mongoose
+                    </li>
+                    <li>
+                        REST
+                    </li>
+                    <li>
+                        GraphQL
+                    </li>
+                    <li>
+                        OOP
+                    </li>
+                    <li>
+                        JSON
+                    </li>
+                    <li>
+                        Inquirer
+                    </li>
+                    <li>
+                        Handlebars
+                    </li>
+                </ul>
+                </div>
+                <div className="list-container">
+                <h2>Other Relevant Skills</h2>
+                <ul>
+                    <li>
+                        Git/GitHub/Git Bash
+                    </li>
+                    <li>
+                        VS Code
+                    </li>
+                    <li>
+                        Heroku
+                    </li>
+                    <li>
+                        Google Maps APIs
+                    </li>                    
+                </ul>
+                </div>
+            </div>
+        </section>
     )
 }
 
