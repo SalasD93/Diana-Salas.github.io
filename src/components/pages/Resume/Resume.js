9bc8ae38aab3b810cc71045510756108bec133ec
Diana Salas <salasd10418@gmail.com>
2021-11-03 01:05:30 -0400

    added resume to Resume page

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Pdf from './Documents/Diana_Salas_Resume_Updated.pdf';
 // import {} from './ResumeStyles';
 
 function Resume() {
     return (
         <div>
             Resume
+            <a href={Pdf} target="_blank" rel="noreferrer">Download my full resume</a>
         </div>
     )
 }
