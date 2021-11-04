6cd096441c1a1e2264c7228e0011346b90942ba3
Diana Salas <salasd10418@gmail.com>
2021-11-04 01:56:03 -0400

    added mobile responsiveness to porfolio page

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -11,7 +11,7 @@ function Project(props) {
                 <LazyLoad offset={100} className="project-cards" key={project._id}>
                     <img id="project-img" src={project.imgUrl} alt={project.title} style={project.style} />
                     {project.deployedLink ? <a href={project.deployedLink}><h2 className="show">{project.title}</h2></a> : <a href={project.videoLink}><h2 className="show">{project.title}</h2></a>}
-                    <a className="show" href= {project.repositoryLink}><FontAwesomeIcon className="fa-5x" icon={faGithub} /></a>
+                    <a className="show" href= {project.repositoryLink}><FontAwesomeIcon className="fa-4x" icon={faGithub} /></a>
                     <h3 className="show">{project.description}</h3>
                 </LazyLoad>
             ))}
