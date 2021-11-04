14e4283837b6bb07810edd9f02f4d0811925a80f
Diana Salas <salasd10418@gmail.com>
2021-11-03 23:34:40 -0400

    updated image sizes

--- src/projectList.js
+++ src/projectList.js
@@ -8,7 +8,7 @@ const projects = [
     {
         _id: "1",
         imgUrl: project1Img,
-        style: {width: "400px", height: "200px"},
+        style: {width: "600px", height: "350px"},
         title: "IDK, You Pick!",
         deployedLink: "https://salasd93.github.io/idk-you-pick/",
         repositoryLink: "https://github.com/SalasD93/idk-you-pick"
@@ -16,7 +16,7 @@ const projects = [
     {
         _id: "2",
         imgUrl: project2Img,
-        style: {width: "400px", height: "200px"},
+        style: {width: "600px", height: "350px"},
         title: "BookMe",
         deployedLink: "https://bookmebuyme.herokuapp.com/",
         repositoryLink: "https://github.com/SalasD93/bookme"
@@ -24,7 +24,7 @@ const projects = [
     {
         _id: "3",
         imgUrl: weatherImg,
-        style: {width: "400px", height: "200px"},
+        style: {width: "600px", height: "350px"},
         title: "What's the Weather Like?",
         deployedLink: "https://salasd93.github.io/whats-the-weather-like/",
         repositoryLink: "https://github.com/SalasD93/whats-the-weather-like"
@@ -32,7 +32,7 @@ const projects = [
     {
         _id: "4",
         imgUrl: teamProfile,
-        style: {width: "400px", height: "200px"},
+        style: {width: "600px", height: "350px"},
         title: "Generate a Team Profile",
         videoLink: "https://drive.google.com/file/d/1RT5vH1dW-Y7bDf8vSYyfE0HQHLSvCQDy/view",
         repositoryLink: "https://github.com/SalasD93/generate-a-team-profile"
@@ -40,7 +40,7 @@ const projects = [
     {
         _id: "5",
         imgUrl: schedulerImg,
-        style: {width: "400px", height: "200px"},
+        style: {width: "600px", height: "350px"},
         title: "Work Day Scheduler",
         deployedLink: "https://salasd93.github.io/work-day-scheduler-ch5/",
         repositoryLink: "https://github.com/SalasD93/work-day-scheduler-ch5"
